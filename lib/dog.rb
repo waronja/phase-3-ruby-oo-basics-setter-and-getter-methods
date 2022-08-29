46d3ff16cab60c003d04e90dd40e77d6f6b422df
simon waronja <simon.waronja@student.moringaschool.com>
2022-08-29 15:24:41 +0300

    done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
 class Dog
+    # attr_accessor :name
+    # attr_accessor :breed
+    def name=(name)
+        @name = name
+    end
+    def name
+        @name
+    end
 
+    def breed=(breed)
+        @breed = breed
+    end
+     def breed
+        @breed
+    end
 end
+
+fido= Dog.new
+fido.name = "Fido"
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,24 @@
 class Person
+    # attr_accessor :name
+    # attr_accessor :job
+    def name=(name)
+        @name=name
+    end
+
+    def name
+        @name
+    end
+
+    def job=(job)
+        @job = job
+    end
+
+    def job
+        @job
+    end
+
 
 end
+
+snoopy = Dog.new
+snoopy.breed = "Beagle"
